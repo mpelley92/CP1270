d42e6a5e0bda1d3f791921fc01391a70b115d7e8
mpelley92 <47612684+mpelley92@users.noreply.github.com>
2019-05-30 15:58:18 -0230

    Create eight.java

---
+++ eight.java
@@ -0,0 +1,14 @@
+package core;
+
+import java.util.*;
+
+public class eight {
+	public static void main() {
+		Scanner input = new Scanner(System.in);
+		int first, second, third;
+		first = 3;
+		second = 6;
+		third = first + second;
+		System.out.println(third);
+	}
+}
